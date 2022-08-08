bac4da7af1e45aa3b32d84edef20ca8272eb031a
DevSlem <devslem12@gmail.com>
2022-08-09 04:23:21 +0900

    Update EpisodicTraejctory

--- aine_drl/experience/episodic_trajectory.py
+++ aine_drl/experience/episodic_trajectory.py
@@ -8,6 +8,7 @@ class EpisodicTrajectory(Trajectory):
     It's a trajectory for Monte Carlo methods.
     """
     def __init__(self, env_count: int = 1) -> None:
+        assert env_count > 0
         self.env_count = env_count
         self.reset()
         
@@ -19,18 +20,20 @@ class EpisodicTrajectory(Trajectory):
     @aine_api
     @property
     def can_train(self) -> bool:
+        # if at least one trajectory has been terminated
         return np.array(self._can_train).sum() > 0
         
     @aine_api
     def reset(self):
-        self._count = 0
+        self._count = 0 # total experience count of all environments
         self._can_train = [False] * self.env_count
         
+        # each list shape: (env count, trajectory length of each env)
         self.states = [[] for i in range(self.env_count)]
         self.actions = [[] for i in range(self.env_count)]
         self.rewards = [[] for i in range(self.env_count)]
         self.terminateds = [[] for i in range(self.env_count)]
-        self.next_state_buffer = [None] * self.env_count
+        self.next_state_buffer = [None] * self.env_count # most recently added next state
         
     @aine_api
     def add(self, experiences: Union[Experience, List[Experience]]):
@@ -52,6 +55,7 @@ class EpisodicTrajectory(Trajectory):
     @aine_api
     def sample(self) -> ExperienceBatch:
         for i, can_train in enumerate(self._can_train):
+            # if ith trajectory has been terminated, then immediately sample an experience batch (i.e. whole trajectory) and return it. 
             if can_train:
                 self.states[i].append(self.next_state_buffer[i])
                 experience_batch = ExperienceBatch(
