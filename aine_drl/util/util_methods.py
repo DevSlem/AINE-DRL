cfe0625bd3da45353edb0450e6a1328448071258
DevSlem <devslem12@gmail.com>
2022-08-09 22:04:52 +0900

    Bug fix `util.get_batch()`

--- aine_drl/util/util_methods.py
+++ aine_drl/util/util_methods.py
@@ -1,17 +1,18 @@
 import operator
 import numpy as np
 
-def get_batch(arr, idxs: np.ndarray) -> np.ndarray:
-    """Returns a batch that consists of elements for the indexes from the array.
+def get_batch(arr, idxs) -> np.ndarray:
+    """
+    Returns a batch that consists of elements for the indexes from the array.
 
     Args:
-        arr (ArrayLike): array
-        idxs (np.ndarray): indexes
+        arr (Any): array
+        idxs (Any): indexes
 
     Returns:
         np.ndarray: batch from the array
     """
     if isinstance(arr, np.ndarray):
-        return np.array(operator.itemgetter(*idxs)(arr))
-    else:
         return arr[idxs]
+    else:
+        return np.array(operator.itemgetter(*idxs)(arr))
