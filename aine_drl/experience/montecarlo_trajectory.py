af31dca9f8276aaf3993775a101df043393fe1cd
DevSlem <devslem12@gmail.com>
2022-08-11 02:50:15 +0900

    Update `MonteCarloTrajectory`
    It's now able to store and sample trajectories of multiple environments.

--- aine_drl/experience/montecarlo_trajectory.py
+++ aine_drl/experience/montecarlo_trajectory.py
@@ -1,15 +1,16 @@
 from aine_drl.experience import Experience, ExperienceBatch, Trajectory
-from typing import Union, List
+from typing import List
 from aine_drl import aine_api
 import numpy as np
 
 class MonteCarloTrajectory(Trajectory):
     """
-    It's a simple trajectory for on-policy Monte Carlo methods. It allows only one environment.
-    It samples whole trajectory when the trajectory is terminated.
+    It's a trajectory for on-policy Monte Carlo methods.
+    It samples whole trajectories when the episode is terminated, so the returned trajectory is the episode of the environment.
     """
-    def __init__(self) -> None:
+    def __init__(self, env_count: int = 1) -> None:
         self.reset()
+        self.env_count = env_count
         
     @aine_api
     @property
@@ -19,49 +20,59 @@ class MonteCarloTrajectory(Trajectory):
     @aine_api
     @property
     def can_train(self) -> bool:
-        return self._can_train
+        # if all episodes are terminated
+        return np.array(self._can_train).sum() == self.env_count
         
     @aine_api
     def reset(self):
         self._count = 0 # total experience count
-        self._can_train = False
+        self._can_train = [False] * self.env_count
+        self.returned_trajectory = 0
         
-        self.states = []
-        self.actions = []
-        self.rewards = []
-        self.terminateds = []
-        self.next_state_buffer = None
+        # shape: (env_count, episode length)
+        self.states = [[] for _ in range(self.env_count)]
+        self.actions = [[] for _ in range(self.env_count)]
+        self.rewards = [[] for _ in range(self.env_count)]
+        self.terminateds = [[] for _ in range(self.env_count)]
+        self.next_state_buffer = [None] * self.env_count
         
     @aine_api
-    def add(self, experiences: Union[Experience, List[Experience]]):
-        if isinstance(experiences, Experience):
-            experiences = [experiences]
+    def add(self, experiences: List[Experience]):
+        assert len(experiences) == self.env_count
+        for i, ex in enumerate(experiences):
+            # if the episode of the environment has been terminated, skip
+            if self._can_train[i]:
+                continue
             
-        # it allows only one environment
-        assert len(experiences) == 1
-        ex = experiences[0]
-        
-        self._count += 1
-        self._can_train = ex.terminated
-        
-        self.states.append(ex.state)
-        self.actions.append(ex.action)
-        self.rewards.append(ex.reward)
-        self.terminateds.append(ex.terminated)
-        self.next_state_buffer = ex.next_state
+            self._count += 1
+            self._can_train[i] = ex.terminated
+            
+            self.states[i].append(ex.state)
+            self.actions[i].append(ex.action)
+            self.rewards[i].append(ex.reward)
+            self.terminateds[i].append(ex.terminated)
+            self.next_state_buffer[i] = ex.next_state
             
     @aine_api
     def sample(self) -> ExperienceBatch:
-        # if the trajectory has been terminated, then sample an experience batch (i.e. whole trajectory) and return it.
-        self.states.append(self.next_state_buffer)
+        """
+        Returns each trajectory, that is an episode. You need to call it while can train.
+
+        Returns:
+            ExperienceBatch: an episode
+        """
+        i = self.returned_trajectory
+        self.states[i].append(self.next_state_buffer)
         experience_batch = ExperienceBatch.create(
-            self.states[:-1], # states
-            self.actions,
-            self.states[1:], # next states
-            self.rewards,
-            self.terminateds
+            self.states[i][:-1], # states
+            self.actions[i],
+            self.states[i][1:], # next states
+            self.rewards[i],
+            self.terminateds[i]
         )
-        # because it's on-policy method, all experiences which are used to train must be discarded.
-        self.reset()
+        self.returned_trajectory += 1
+        # if all trajectories has been returned, then reset
+        if self.returned_trajectory == self.env_count:
+            self.reset()
         return experience_batch
     
