3d9056b771ae3666be87a4069883e1fc79cf36e3
DevSlem <devslem12@gmail.com>
2022-08-11 23:15:45 +0900

    Add `Policy` abstract class

---
+++ aine_drl/policy/policy.py
@@ -0,0 +1,30 @@
+from abc import ABC, abstractmethod
+from aine_drl.util import aine_api
+import torch
+from torch.distributions import Distribution
+
+class Policy(ABC):
+    
+    @aine_api
+    @abstractmethod
+    def get_policy_distribution(pdparam: torch.Tensor) -> Distribution:
+        """
+        Returns policy distribution that is action probability distribution.
+
+        Args:
+            pdparam (torch.Tensor): policy distribution parameter that is noramlly the output of neural network
+
+        Returns:
+            Distribution: policy distribution
+        """
+        raise NotImplementedError
+    
+    @aine_api
+    def update_hyperparams(time_step: int):
+        """
+        Update hyperparameters if they exists.
+
+        Args:
+            time_step (int): current time step during training
+        """
+        pass
