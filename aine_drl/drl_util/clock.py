56a855a8d2eb052d16c89421213d5414a87785d5
DevSlem <devslem12@gmail.com>
2022-08-12 20:43:40 +0900

    Update `Clock`

--- aine_drl/drl_util/clock.py
+++ aine_drl/drl_util/clock.py
@@ -5,14 +5,14 @@ class Clock:
     Don't use it yet. It needs to be implemented.
     """
     
-    def __init__(self) -> None:
-        raise NotImplementedError
+    def __init__(self, env_count: int) -> None:
+        self.env_count = env_count
         self.reset()
     
     def reset(self):
         self._time_step = 0
         self._episode = 0
-        self._total_time_step = 0
+        self._episode_len = 0
         self._real_start_time = time.time()
         self._real_time = 0.0
         self._training_step = 0
@@ -26,8 +26,8 @@ class Clock:
         return self._episode
     
     @property
-    def total_time_step(self) -> int:
-        return self._total_time_step
+    def episode_len(self) -> int:
+        return self._episode_len
     
     @property
     def training_step(self) -> int:
@@ -38,12 +38,12 @@ class Clock:
         return int(self._real_time)
     
     def tick_time_step(self):
-        self._time_step += 1
-        self._total_time_step += 1
+        self._episode_len += 1
+        self._time_step += self.env_count
         self._real_time = self._get_real_time()
         
     def tick_episode(self):
-        self._time_step = 0
+        self._episode_len = 0
         self._episode += 1
         
     def tick_training_step(self):
