525256a1d31c492f7339abb4264c69c72f783b9c
DevSlem <devslem12@gmail.com>
2022-09-15 03:05:30 +0900

    Update drl_util

--- aine_drl/drl_util/drl_util_methods.py
+++ aine_drl/drl_util/drl_util_methods.py
@@ -11,3 +11,15 @@ def polyak_update(src_net: nn.Module, target_net: nn.Module, src_ratio: float):
     assert src_ratio >= 0 and src_ratio <= 1
     for src_param, target_param in zip(src_net.parameters(), target_net.parameters()):
         target_param.data.copy_(src_ratio * src_param.data + (1.0 - src_ratio) * target_param.data)
+
+def calc_returns(rewards: torch.Tensor, terminateds: torch.Tensor, gamma: float):
+    """
+    Calculates returns. `rewards`, `terminateds` can be multiple episodes but must be flattend.
+    """
+    returns = torch.empty_like(rewards)
+    G = 0 # return at time step t
+    not_terminateds = 1 - terminateds
+    for t in reversed(range(len(returns))):
+        G = rewards[t] + not_terminateds[t] * gamma * G
+        returns[t] = G
+    return returns
