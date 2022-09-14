0529a8a087429302e9e9352223182718b531b050
DevSlem <devslem12@gmail.com>
2022-09-15 03:06:58 +0900

    Add `REINFORCE` test code

--- test/cartpole_v1_dqn.py
+++ test/cartpole_v1_dqn.py
@@ -30,17 +30,22 @@ class QValueNet(nn.Module):
 
 def main():
     seed = 0 # if you want to get the same results
+    venv_mode = True
+    
     util.seed(seed)
     total_training_step = 150000
-    training_freq = 16
-    num_envs = 3
-    env = gym.vector.make("CartPole-v1", num_envs=num_envs, new_step_api=True)
-    obs_shape = env.single_observation_space.shape[0]
-    action_count = env.single_action_space.n
-    # num_envs = 1
-    # env = gym.make("CartPole-v1", new_step_api=True)
-    # obs_shape = env.observation_space.shape[0]
-    # action_count = env.action_space.n
+    training_freq = 32
+    
+    if venv_mode:
+        num_envs = 3
+        env = gym.vector.make("CartPole-v1", num_envs=num_envs, new_step_api=True)
+        obs_shape = env.single_observation_space.shape[0]
+        action_count = env.single_action_space.n
+    else:
+        num_envs = 1
+        env = gym.make("CartPole-v1", new_step_api=True)
+        obs_shape = env.observation_space.shape[0]
+        action_count = env.action_space.n
     
     device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
     q_net = QValueNet(obs_shape, action_count).to(device=device)
@@ -53,7 +58,7 @@ def main():
     )
     clock = aine_drl.Clock(num_envs)
     epsilon_greedy = aine_drl.EpsilonGreedyPolicy(aine_drl.LinearDecay(0.3, 0.01, 0, total_training_step * 0.75))
-    exp_replay = aine_drl.ExperienceReplay(training_freq, 32, 5000, num_envs)
+    exp_replay = aine_drl.ExperienceReplay(training_freq, 32, 1000, num_envs)
     dqn = aine_drl.DoubleDQN(
         dqn_spec,
         epsilon_greedy,
@@ -62,7 +67,7 @@ def main():
         gamma=0.99,
         epoch=3,
         summary_freq=1000,
-        update_freq=256
+        update_freq=128
     )
     gym_training = GymTraining(dqn, env, seed=seed)
     gym_training.run_train(total_training_step)
