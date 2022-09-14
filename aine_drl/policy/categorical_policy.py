e69da2f3dc53ff07a20578a6cdc0e166fb6a4455
DevSlem <devslem12@gmail.com>
2022-09-15 03:05:47 +0900

    Update `CategoricalPolicy`

--- aine_drl/policy/categorical_policy.py
+++ aine_drl/policy/categorical_policy.py
@@ -8,6 +8,12 @@ class CategoricalPolicy(Policy):
     Simple categorical policy for the discrete action.
     """
     
+    def __init__(self, is_probs: bool = False) -> None:
+        self.is_probs = is_probs
+    
     @aine_api
     def get_policy_distribution(self, pdparam: torch.Tensor) -> Distribution:
-        return Categorical(logits=pdparam)
+        if self.is_probs:
+            return Categorical(probs=pdparam)
+        else:
+            return Categorical(logits=pdparam)
