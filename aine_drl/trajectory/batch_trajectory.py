794c24dd04a9277be4532b9b4c0db3072fb30448
DevSlem <devslem12@gmail.com>
2022-11-05 18:07:59 +0900

    Refactor Trajectory

--- aine_drl/trajectory/batch_trajectory.py
+++ aine_drl/trajectory/batch_trajectory.py
@@ -1,45 +1,54 @@
-from typing import List
-from aine_drl.drl_util import Experience
-from aine_drl.trajectory import Trajectory
+from aine_drl.experience import Action, Experience, ExperienceBatchTensor
+import torch
+import numpy as np
 
-class BatchTrajectory(Trajectory):
+class BatchTrajectory:
     """
-    It's a batch trajectory abstract class for the batch learning. It can store `max_num_exp` experiences.
+    It's a trajectory utility of experience batch for the batch learning.
+    
+    Args:
+        max_n_steps (int): maximum number of time steps to be stored
     """
-    def __init__(self, max_num_exp: int, num_envs: int = 1) -> None:
-        """
-        Args:
-            max_num_exp (int): maximum number of experiences to be stored
-            num_envs (int, optional): number of environments. Defaults to 1.
-        """
-        assert max_num_exp > 0 and num_envs > 0
-        self.num_envs = num_envs
-        self.max_num_exp = max_num_exp # maximum number of elements
+    def __init__(self, max_n_steps: int) -> None:
+        self.max_n_steps = max_n_steps
         self.reset()
         
     @property
     def count(self) -> int:
         return self._count
+    
+    @property
+    def recent_idx(self) -> int:
+        return self._recent_idx
         
     def reset(self):
-        self._count = 0 # total experience count of all environments
-        self.recent_idx = -1 # index of the most recent experience
+        self._count = 0
+        self._recent_idx = -1
         
-        self.states = [None] * self.max_num_exp
-        self.actions = [None] * self.max_num_exp
-        self.rewards = [None] * self.max_num_exp
-        self.terminateds = [None] * self.max_num_exp
-        self.next_state_buffer = [None] * self.num_envs # most recently added next state
+        self.obs = [None] * self.max_n_steps
+        self.action = [None] * self.max_n_steps
+        self.reward = [None] * self.max_n_steps
+        self.terminated = [None] * self.max_n_steps
+        self.next_obs_buffer = None # most recently added next state
         
-    def add(self, experiences: List[Experience]):
-        assert len(experiences) == self.num_envs
-        for i, ex in enumerate(experiences):
-            self.recent_idx = (self.recent_idx + 1) % self.max_num_exp
-            self._count = min(self._count + 1, self.max_num_exp)
-            
-            self.states[self.recent_idx] = ex.state
-            self.actions[self.recent_idx] = ex.action
-            self.rewards[self.recent_idx] = ex.reward
-            self.terminateds[self.recent_idx] = ex.terminated
-            self.next_state_buffer[i] = ex.next_state
+    def add(self, experience: Experience):
+        self._recent_idx += (self._recent_idx + 1) % self.max_n_steps
+        self._count = min(self._count + 1, self.max_n_steps)
+        
+        self.obs[self._recent_idx] = experience.obs
+        self.action[self._recent_idx] = experience.action
+        self.reward[self._recent_idx] = experience.reward
+        self.terminated[self._recent_idx] = experience.terminated
+        self.next_obs_buffer = experience.next_obs
     
+    def sample(self) -> ExperienceBatchTensor:
+        self.obs.append(self.next_obs_buffer)
+        exp_batch = ExperienceBatchTensor(
+            torch.from_numpy(np.concatenate(self.obs[:-1], axis=0)),
+            Action.to_batch(self.action).to_action_tensor(),
+            torch.from_numpy(np.concatenate(self.obs[1:], axis=0)),
+            torch.from_numpy(np.concatenate(self.reward, axis=0)),
+            torch.from_numpy(np.concatenate(self.terminated, axis=0)),
+            self.count
+        )
+        return exp_batch
