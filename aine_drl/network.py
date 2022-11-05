ffedf7c5fcfa3d4ad9c3ce53e30db0aff7d61d20
DevSlem <devslem12@gmail.com>
2022-11-06 02:48:47 +0900

    Update `ActorCriticSharedNetwork`

--- aine_drl/network.py
+++ aine_drl/network.py
@@ -1,5 +1,6 @@
 from abc import ABC, abstractmethod
 from typing import Optional, Tuple
+from aine_drl.policy.policy_distribution import PolicyDistributionParameter
 import torch
 import torch.nn as nn
 
@@ -10,18 +11,19 @@ class ActorCriticSharedNetwork(nn.Module, ABC):
     
     @abstractmethod
     def forward(self, 
-                obs: torch.Tensor) -> Tuple[torch.Tensor, torch.Tensor]:
+                obs: torch.Tensor) -> Tuple[PolicyDistributionParameter, torch.Tensor]:
         """
-        Calculate policy distribution paraemters whose shape is (batch_size, ...), state value whose shape is (batch_size, 1), next_hidden_state which is (h, c) tuple.
-        batch_size is generally (num_envs * n-step).
-        If action type is discrete, policy distribution is generally logits or softmax distribution.
-        If action type is continuous, it's generally mean and standard deviation of gaussian distribution.
+        Calculate policy distribution paraemters whose shape is `(batch_size, ...)`, 
+        state value whose shape is `(batch_size, 1)`. \\
+        `batch_size` is `num_envs` x `n-step`. \\
+        When the action type is discrete, policy distribution is generally logits or soft-max distribution. \\
+        When the action type is continuous, it's generally mean and standard deviation of gaussian distribution.
 
         Args:
-            obs (torch.Tensor): observation of state whose shape is (batch_size, *obs_shape)
+            obs (torch.Tensor): observation of state whose shape is `(batch_size, *obs_shape)`
 
         Returns:
-            Tuple[torch.Tensor, torch.Tensor]: policy distribution parameter, state value
+            Tuple[PolicyDistributionParameter, torch.Tensor]: policy distribution parameter, state value
         """
         raise NotImplementedError
     
