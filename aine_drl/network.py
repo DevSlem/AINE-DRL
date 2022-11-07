e03dc31a51a2f96d98b02e4d13cb229f856244f2
DevSlem <devslem12@gmail.com>
2022-11-07 14:09:46 +0900

    Add action layer utility

--- aine_drl/network.py
+++ aine_drl/network.py
@@ -1,8 +1,104 @@
 from abc import ABC, abstractmethod
-from typing import Optional, Tuple
+from typing import Optional, Tuple, Union, Any
 from aine_drl.policy.policy_distribution import PolicyDistributionParameter
 import torch
 import torch.nn as nn
+import torch.nn.functional as F
+
+class DiscreteActionLayer(nn.Module):
+    """
+    Linear layer for the discrete action type.
+
+    Args:
+        in_features (int): number of input features
+        num_discrete_actions (int | Tuple[int, ...]): each element indicates number of discrete actions of each branch
+        is_logits (bool): whether logits or probabilities. Defaults to logits.
+    """
+
+    def __init__(self, in_features: int, 
+                 num_discrete_actions: Union[int, Tuple[int, ...]], 
+                 is_logits: bool = True,
+                 bias: bool = True,
+                 device: Optional[torch.device] = None,
+                 dtype: Optional[Any] = None) -> None:
+        """
+        Linear layer for the discrete action type.
+
+        Args:
+            in_features (int): number of input features
+            num_discrete_actions (int | Tuple[int, ...]): each element indicates number of discrete actions of each branch
+            is_logits (bool): whether logits or probabilities. Defaults to logits.
+        """
+        super().__init__()
+        
+        self.is_logits = is_logits
+        
+        if type(num_discrete_actions) is int:
+            num_discrete_actions = (num_discrete_actions,)
+        self.num_discrete_actions = num_discrete_actions
+        
+        self.total_num_discrete_actions = 0
+        for num_action in num_discrete_actions:
+            self.total_num_discrete_actions += num_action
+        
+        self.layer = nn.Linear(
+            in_features,
+            self.total_num_discrete_actions,
+            bias,
+            device,
+            dtype
+        )
+    
+    def forward(self, x: torch.Tensor) -> PolicyDistributionParameter:
+        out = self.layer(x)
+        discrete_pdparams = list(torch.split(out, self.num_discrete_actions, dim=1))
+        
+        if not self.is_logits:
+            for i in range(len(discrete_pdparams)):
+                discrete_pdparams[i] = F.softmax(discrete_pdparams[i], dim=1)
+        
+        return PolicyDistributionParameter.create(discrete_pdparams, None)
+    
+class GaussianContinuousActionLayer(nn.Module):
+    """
+    Linear layer for the continuous action type.
+
+    Args:
+        in_features (int): number of input features
+        num_continuous_actions (int): number of continuous actions which equals to `num_continuous_branches`
+    """
+    
+    def __init__(self, in_features: int, 
+                 num_continuous_actions: int, 
+                 bias: bool = True,
+                 device: Optional[torch.device] = None,
+                 dtype: Optional[Any] = None) -> None:
+        """
+        Linear layer for the continuous action type.
+
+        Args:
+            in_features (int): number of input features
+            num_continuous_actions (int): number of continuous actions which equals to `num_continuous_branches`
+        """
+        super().__init__()
+        
+        self.num_continuous_actions = num_continuous_actions
+        
+        self.layer = nn.Linear(
+            in_features,
+            self.num_continuous_actions * 2,
+            bias,
+            device,
+            dtype
+        )
+        
+    def forward(self, x: torch.Tensor) -> PolicyDistributionParameter:
+        out = self.layer(x)
+        out = torch.reshape(out, (-1, self.num_continuous_actions, 2))
+        torch.abs_(out[..., 1])
+        out = torch.reshape(out, (-1, self.num_continuous_actions * 2))
+        continuous_pdparams = list(torch.split(out, 2, dim=1))
+        return PolicyDistributionParameter.create(None, continuous_pdparams)
 
 class ActorCriticSharedNetwork(nn.Module, ABC):
     """
