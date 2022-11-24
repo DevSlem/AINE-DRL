aa666033c62773a7df6077344202becd7e43ec84
DevSlem <devslem12@gmail.com>
2022-11-24 20:36:30 +0900

    Update sample config and codes

--- samples/cartpole_v1_ppo.py
+++ samples/cartpole_v1_ppo.py
@@ -2,22 +2,21 @@ import sys
 sys.path.append(".")
 
 from typing import Optional, Tuple
-
 import aine_drl
 import aine_drl.util as util
-from aine_drl.training import GymTraining
-
 import torch
 import torch.nn as nn
 import torch.optim as optim
 
 class CartPoleActorCriticNet(aine_drl.ActorCriticSharedNetwork):
+    # PPO uses ActorCriticSharedNetwork.
     
     def __init__(self, obs_shape, discrete_action_count) -> None:
         super().__init__()
         
         self.hidden_feature = 64
         
+        # encoding layer for shared network
         self.encoding_layer = nn.Sequential(
             nn.Linear(obs_shape, 128),
             nn.ReLU(),
@@ -25,11 +24,14 @@ class CartPoleActorCriticNet(aine_drl.ActorCriticSharedNetwork):
             nn.ReLU()
         )
         
+        # actor-critic layer
         self.actor_layer = aine_drl.DiscreteActionLayer(self.hidden_feature, discrete_action_count)
         self.critic_layer = nn.Linear(self.hidden_feature, 1)
         
+        # optimizer for this network
         self.optimizer = optim.Adam(self.parameters(), lr=0.001)
     
+    # override
     def forward(self, obs: torch.Tensor) -> Tuple[aine_drl.PolicyDistributionParameter, torch.Tensor]:
         encoding = self.encoding_layer(obs)
         pdparam = self.actor_layer(encoding)
@@ -37,29 +39,34 @@ class CartPoleActorCriticNet(aine_drl.ActorCriticSharedNetwork):
         
         return pdparam, v_pred
     
+    # override
     def train_step(self, loss: torch.Tensor, grad_clip_max_norm: Optional[float], training_step: int):
         self.basic_train_step(loss, self.optimizer, grad_clip_max_norm)
     
-def main():
+if __name__ == "__main__":
     seed = 0 # if you want to get the same results
     util.seed(seed)
     
-    config_manager = aine_drl.util.ConfigManager("config/cartpole_v1_ppo.yaml")
-    gym_training = GymTraining.make(config_manager.env_config, config_manager.env_id)
+    # AINE-DRL configuration manager
+    aine_config = aine_drl.AINEConfig("config/cartpole_v1_ppo.yaml")
     
-    if gym_training.is_vector_env:
-        obs_shape = gym_training.gym_env.single_observation_space.shape[0]
-        action_count = gym_training.gym_env.single_action_space.n
-    else:
-        obs_shape = gym_training.gym_env.observation_space.shape[0]
-        action_count = gym_training.gym_env.action_space.n
+    # make gym training instance
+    gym_training = aine_config.make_gym_training()
     
+    # create custom network
+    obs_shape = gym_training.observation_space.shape[0]
+    action_count = gym_training.action_space.n
     device = None #torch.device("cuda" if torch.cuda.is_available() else "cpu")
     network = CartPoleActorCriticNet(obs_shape, action_count).to(device=device)
-    policy = aine_drl.CategoricalPolicy()
-    ppo = aine_drl.PPO.make(config_manager.env_config, network, policy)
-    gym_training.train(ppo)
-    gym_training.close()
     
-if __name__ == "__main__":
-    main()
+    # create policy for discrete action type
+    policy = aine_drl.CategoricalPolicy()
+    
+    # make PPO agent
+    ppo = aine_config.make_agent(network, policy)
+    
+    # training start!
+    gym_training.train(ppo)
+    
+    # training close safely
+    gym_training.close()
