1c4380907ebb5c3a3505444928e91fc479e28bb5
DevSlem <devslem12@gmail.com>
2022-11-26 17:07:36 +0900

    Update `AINEConfig`

--- aine_drl/aine_config.py
+++ aine_drl/aine_config.py
@@ -10,6 +10,9 @@ from gym import Env
 from gym.vector import VectorEnv
 
 class AINEConfig:
+    """
+    AINE-DRL configuration helper class. It parse YAML format configuration and makes training instance, agent, etc.
+    """
     def __init__(self, config_dir: str) -> None:
         self._config = AINEConfig.import_config(config_dir)
         
@@ -20,14 +23,17 @@ class AINEConfig:
         
     @property
     def training_env_id(self) -> str:
+        """Training environment id."""
         return self._training_env_id
         
     @property
     def training_env_config(self) -> dict:
+        """Training environment configuration."""
         return self._config[self.training_env_id]
     
     @property
     def num_envs(self) -> int:
+        """The number of environments specified in the configuration."""
         return self._num_envs
     
     def make_gym_training(self,
@@ -36,14 +42,18 @@ class AINEConfig:
         """
         ## Summary
         
-        Make gym training instance from the YAML configuration.
+        Make gym training instance from the YAML configuration. 
+        The only main key is `Gym` and two sub keys are `env` which specify to make a gym environment and `training` which specify training configurations.
+        Configurations of `env` key are parameters of `gym.make()` function. 
+        Configurations of `training` key are fields of `GymTrainingConfig` class.
 
         Args:
             gym_env (Env | VectorEnv | None, optional): gym environment. Defaults to making it from the configuration.
             gym_action_communicator (GymActionCommunicator | None, optional): action communicator between AINE-DRL and Gym. Defaults to auto set.
 
         Raises:
-            ValueError: When you use custom `gym_env`, `num_envs` in the configuration must equal to the number of environments in `gym_env`.
+            ValueError: When you use custom `gym_env` and the type is `VectorEnv`, `num_envs` in the configuration must equal to the number of environments in `gym_env`.
+            ValueError: When you use custom `gym_env` and the type is `Env`, `num_envs` in the configuration must equal to `1`.
 
         Returns:
             GymTraining: `GymTraining` instance
@@ -80,6 +90,36 @@ class AINEConfig:
         )
     
     def make_agent(self, network: Network, policy: Policy) -> Agent:
+        """
+        ## Summary
+    
+        Make an agent from the YAML configuration. 
+        The main key must be the agent name like `PPO` and at least one agent key must be included. 
+        See configuration details in AINE-DRL Wiki - https://github.com/DevSlem/AINE-DRL/wiki. \\
+        The `network` type must be the type specified for each agent. 
+        For example, the network type of `PPO` agent is `ActorCriticSharedNetwork`.
+        
+        network (Network): network
+        policy (Policy): policy
+        
+        Returns:
+            Agent: `Agent` instance
+            
+        ## Example
+        
+        YAML format::
+        
+            PPO:
+              training_freq: 256
+              epoch: 4
+              mini_batch_size: 128
+              gamma: 0.99
+              lam: 0.95
+              epsilon_clip: 0.2
+              value_loss_coef: 0.2
+              entropy_coef: 0.001
+              grad_clip_max_norm: 5.0
+        """
         return make_agent(self.training_env_config, network, policy, self.num_envs)
         
     @staticmethod
