61e1dc6444c974f8ef90f744f749bea7377824ba
DevSlem <devslem12@gmail.com>
2022-11-27 15:25:14 +0900

    Update policy module

--- aine_drl/policy/__init__.py
+++ aine_drl/policy/__init__.py
@@ -1,1 +1,9 @@
-from .policy import *
+from .policy_distribution import PolicyDistributionParameter
+from .policy import (
+    Policy,
+    CategoricalPolicy,
+    GaussianPolicy,
+    GeneralPolicy,
+    EpsilonGreedyPolicy,
+    BoltzmannPolicy
+)
