728adacc7637469514dde0a17291d6ab10644bdb
DevSlem <devslem12@gmail.com>
2022-11-27 15:26:23 +0900

    Update trajectory module

--- aine_drl/trajectory/__init__.py
+++ aine_drl/trajectory/__init__.py
@@ -1,3 +1,2 @@
-from .montecarlo_trajectory import *
-from .batch_trajectory import *
-from .experience_replay import *
+from .montecarlo_trajectory import MonteCarloTrajectory
+from .batch_trajectory import BatchTrajectory
