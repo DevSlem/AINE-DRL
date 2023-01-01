723b6e5ba2923f034d903310e8f9da32d6c76775
DevSlem <devslem12@gmail.com>
2023-01-01 18:06:41 +0900

    Update SAC
    it's now forbidden

--- aine_drl/agent/sac/sac.py
+++ aine_drl/agent/sac/sac.py
@@ -17,6 +17,7 @@ class SACConfig(NamedTuple):
     epoch: int
     gamma: float = 0.99
     polyak_ratio: float = 0.005
+    alpha: float = 0.2
     grad_clip_max_norm: Optional[float] = None
 
 class SAC(Agent):
@@ -25,6 +26,8 @@ class SAC(Agent):
                  network: SACNetwork, 
                  policy: Policy, 
                  num_envs: int) -> None:
+        raise NotImplementedError("It's not implemented yet. I've not been able to find the reason why it fails training.")
+        
         if policy.action_type is not ActionType.CONTINUOUS:
             raise TypeError(f"The policy must be continuous action policy, but \"{type(policy).__name__}\" is \"{policy.action_type}\".")
         
@@ -64,13 +67,9 @@ class SAC(Agent):
     def train(self):
         for _ in range(self.config.epoch):
             exp_batch = self.trajectory.sample(device=self.device)
+            self._update_parameters(exp_batch)
             
-            self._train_v_net(exp_batch)
-            self._train_q_net(exp_batch)
-            self._train_actor(exp_batch)
             self.clock.tick_training_step()
-            
-            self._update_v_target_net()
             
     @property
     def log_keys(self) -> Tuple[str, ...]:
@@ -95,41 +94,16 @@ class SAC(Agent):
         dist = self.policy.get_policy_distribution(pdparam)
         return dist.sample()
     
-    def _train_v_net(self, exp_batch: ExperienceBatchTensor):
-        """objective function J_V: 0.5 * (V(s) - Q(s,a) + log(pi(a|s)))^2"""
-        with torch.no_grad():
-            # compute log(pi(a|s))
-            pdparam = self.network.actor.forward(exp_batch.obs)
-            dist = self.policy.get_policy_distribution(pdparam)
-            action_log_prob = dist.log_prob(exp_batch.action)
-            
-            # estimate Q(s,a)
-            q_pred1, q_pred2 = self._estimate_two_continuous_q_value(exp_batch.obs, exp_batch.action)
-            min_q_pred = torch.min(q_pred1, q_pred2)        
-        
-        # estimate V(s), its gradient only flows
-        v_pred = self.network.v_net.forward(exp_batch.obs)
-        
-        # compute objective function: soft value loss
-        soft_value_loss = F.mse_loss(v_pred, min_q_pred - action_log_prob)
-        
-        # gradient step
-        self.network.v_net.train_step(soft_value_loss, self.config.grad_clip_max_norm, self.clock.training_step)
-        
-        # record
-        self.average_soft_value_loss.update(soft_value_loss.item())
-    
-    def _train_q_net(self, exp_batch: ExperienceBatchTensor):
-        """objective function J_Q: 0.5 * (Q(s,a) - q_hat(s,a))^2"""
+    def _update_parameters(self, exp_batch: ExperienceBatchTensor):
+        # === objective function J_Q: 0.5 * (Q(s,a) - q_hat(s,a))^2 ===
         with torch.no_grad():
             # compute q_hat
             not_terminated = 1.0 - exp_batch.terminated
-            v_target = self.v_target_net.forward(exp_batch.next_obs)
-            q_hat = exp_batch.reward + not_terminated * self.config.gamma * v_target 
+            next_v_target = self.v_target_net.forward(exp_batch.next_obs)
+            q_hat = exp_batch.reward + not_terminated * self.config.gamma * next_v_target
             
-        # estimate Q(s,a)
         q_pred1, q_pred2 = self._estimate_two_continuous_q_value(exp_batch.obs, exp_batch.action)
-        
+            
         # compute objective function: td loss
         td_loss1 = F.mse_loss(q_pred1, q_hat)
         td_loss2 = F.mse_loss(q_pred2, q_hat)
@@ -141,16 +115,13 @@ class SAC(Agent):
         # record
         self.average_td_loss1.update(td_loss1.item())
         self.average_td_loss2.update(td_loss2.item())
-    
-    def _train_actor(self, exp_batch: ExperienceBatchTensor):
-        """objective function J_pi: log(pi(a|s)) - Q(s,a)"""
+        
+        # === objective function J_pi: log(pi(a|s)) - Q(s,a) ===
         # sample actions
         pdparam = self.network.actor.forward(exp_batch.obs)
         dist = self.policy.get_policy_distribution(pdparam)
         # reparameterized trick must be used
         sampled_action = dist.sample()
-        
-        # compute log(pi)
         action_log_prob = dist.log_prob(sampled_action)
         
         # estimate Q(s,a)
@@ -158,13 +129,33 @@ class SAC(Agent):
         min_q_pred = torch.min(q_pred1, q_pred2)
         
         # compute objective function: policy kl loss
-        policy_kl_loss = (action_log_prob - min_q_pred).mean()
+        policy_kl_loss = (self.config.alpha * action_log_prob - min_q_pred).mean()
         
         # gradient step
         self.network.actor.train_step(policy_kl_loss, self.config.grad_clip_max_norm, self.clock.training_step)
         
         # record
         self.average_policy_kl_loss.update(policy_kl_loss.item())
+        
+        # === objective function J_V: 0.5 * (V(s) - Q(s,a) + log(pi(a|s)))^2 ===
+        with torch.no_grad():
+            v_target = min_q_pred - (self.config.alpha * action_log_prob)
+        
+        # estimate V(s), its gradient only flows
+        v_pred = self.network.v_net.forward(exp_batch.obs)
+        
+        # compute objective function: soft value loss
+        soft_value_loss = F.mse_loss(v_pred, v_target)
+        
+        # gradient step
+        self.network.v_net.train_step(soft_value_loss, self.config.grad_clip_max_norm, self.clock.training_step)
+        
+        # record
+        self.average_soft_value_loss.update(soft_value_loss.item())
+        
+        # === target network update ===
+        self._update_v_target_net()
+        
         
     def _estimate_two_continuous_q_value(self, obs: torch.Tensor, action: ActionTensor) -> Tuple[torch.Tensor, torch.Tensor]:
         q_pred1 = self.network.q_net1.forward(obs, action.continuous_action)
