e4e393f31c31e98d22715dcf6112e94c777e18d9
DevSlem <devslem12@gamil.com>
2023-01-15 18:17:32 +0900

    Add `StaticRecursiveBuffer`

--- aine_drl/util/__init__.py
+++ aine_drl/util/__init__.py
@@ -1,2 +1,3 @@
 from .util_methods import *
 from .logger import *
+from .static_recursive_buffer import StaticRecursiveBuffer
---
+++ aine_drl/util/static_recursive_buffer.py
@@ -0,0 +1,49 @@
+from typing import Tuple, Dict, Any, List
+
+class StaticRecursiveBuffer:
+    """
+    It allocates multiple buffers whose memories have fixed capacity. 
+    It is recurisve structure which means index where an item is pushed is recurisve. 
+    Specifically, you can add a key-value pair dictionary and each value is added to each buffer of the key.
+    """
+    def __init__(self, keys: Tuple[str, ...], capacity: int) -> None:
+        self._keys = keys
+        self._capacity = capacity
+        self.reset()
+        
+    @property
+    def count(self) -> int:
+        return self._count
+    
+    @property
+    def latest_idx(self) -> int:
+        return self._latest_idx
+    
+    @property
+    def is_full(self) -> bool:
+        return self._count == self._capacity
+    
+    @property
+    def keys(self) -> Tuple[str, ...]:
+        return self._keys
+        
+    @property
+    def latest_item(self) -> Dict[str, Any]:
+        return {key: buffer[self._latest_idx] for key, buffer in self._buffer.items()}
+    
+    @property
+    def buffer_dict(self) -> Dict[str, List[Any]]:
+        return {key: buffer[:self._count] for key, buffer in self._buffer.items()}
+        
+    def reset(self):
+        self._count = 0
+        self._latest_idx = -1
+        
+        self._buffer: Dict[str, List[Any]] = {key: [None] * self._capacity for key in self._keys}
+            
+    def add(self, key_value_pair: dict):
+        self._latest_idx = (self._latest_idx + 1) % self._capacity
+        self._count = min(self._count + 1, self._capacity)
+        
+        for key, value in key_value_pair.items():
+            self._buffer[key][self._latest_idx] = value
