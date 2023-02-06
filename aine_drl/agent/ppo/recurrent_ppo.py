ba595e1ab69fb3ac4203452dae8cb341a078ca4c
DevSlem <devslem12@gamil.com>
2023-02-07 02:50:52 +0900

    Update Recurrent PPO
    refactoring

--- aine_drl/agent/ppo/recurrent_ppo.py
+++ aine_drl/agent/ppo/recurrent_ppo.py
@@ -8,7 +8,6 @@ from .ppo_trajectory import RecurrentPPOExperienceBatch, RecurrentPPOTrajectory
 import aine_drl.drl_util as drl_util
 import aine_drl.util as util
 import torch
-from torch.nn.utils.rnn import pad_sequence
 
 class RecurrentPPOConfig(NamedTuple):
     """
@@ -95,7 +94,7 @@ class RecurrentPPO(Agent):
         
         # if training frequency is reached, start training
         if self.trajectory.count == self.config.training_freq:
-            self.train()
+            self._train()
             
     def inference(self, experience: Experience):
         self.inference_prev_terminated = torch.from_numpy(experience.terminated)
@@ -105,17 +104,17 @@ class RecurrentPPO(Agent):
             self.current_hidden_state = self.next_hidden_state * (1.0 - self.prev_terminated)
             # when interacting with environment, sequence_length must be 1
             # feed forward
-            pdparam, v_pred, hidden_state = self.network.forward(obs.unsqueeze(dim=1), self.current_hidden_state.to(device=self.device))
+            pdparam_seq, state_value_seq, next_hidden_state = self.network.forward(obs.unsqueeze(dim=1), self.current_hidden_state.to(device=self.device))
             
             # action sampling
-            pdparam = pdparam.sequence_to_flattened()
+            pdparam = pdparam_seq.sequence_to_flattened()
             dist = self.policy.get_policy_distribution(pdparam)
             action = dist.sample()
             
             # store data
             self.current_action_log_prob = dist.log_prob(action).cpu()
-            self.v_pred = v_pred.squeeze_(dim=1).cpu()
-            self.next_hidden_state = hidden_state.cpu()
+            self.v_pred = state_value_seq.squeeze_(dim=1).cpu()
+            self.next_hidden_state = next_hidden_state.cpu()
             
             return action
     
@@ -127,7 +126,7 @@ class RecurrentPPO(Agent):
         self.inference_next_hidden_state = hidden_state.cpu()
         return action
             
-    def train(self):
+    def _train(self):
         # sample experience batch from the trajectory
         exp_batch = self.trajectory.sample(self.device)
         
@@ -171,29 +170,29 @@ class RecurrentPPO(Agent):
                 
                 # feed forward
                 # in this case num_seq = num_seq_per_step
-                pdparam, predicted_state_value, _ = self.network.forward(obs_seq[sample_seq], seq_init_hidden_state[:, sample_seq])
+                pdparam_seq, state_value_seq, _ = self.network.forward(obs_seq[sample_seq], seq_init_hidden_state[:, sample_seq])
                 
                 # compute actor loss
                 # (num_seq_per_step, seq_len, *pdparam_shape) -> (num_seq_per_step * seq_len, *pdparam_shape)
-                pdparam = pdparam.sequence_to_flattened()
-                # (num_seq_per_step, seq_len, num_actions) -> (num_seq_per_step * seq_len, num_actions)
+                pdparam = pdparam_seq.sequence_to_flattened()
                 dist = self.policy.get_policy_distribution(pdparam)
+                # (num_seq_per_step, seq_len, num_actions) -> (num_seq_per_step * seq_len, num_actions)
                 a = ActionTensor(
                     discrete_action_seq[sample_seq].flatten(0, 1),
                     continuous_action_seq[sample_seq].flatten(0, 1)
                 )
                 # (num_seq_per_step * seq_len, 1) -> (num_seq_per_step, seq_len, 1)
-                new_action_log_prob = dist.log_prob(a).reshape(self.config.num_sequences_per_step, -1, 1)
+                new_action_log_prob_seq = dist.log_prob(a).reshape(self.config.num_sequences_per_step, -1, 1)
                 actor_loss = PPO.compute_actor_loss(
                     advantage_seq[sample_seq][m],
                     old_action_log_prob_seq[sample_seq][m],
-                    new_action_log_prob[m],
+                    new_action_log_prob_seq[m],
                     self.config.epsilon_clip
                 )
                 entropy = dist.entropy().reshape(self.config.num_sequences_per_step, -1, 1)[m].mean()
                 
                 # compute critic loss
-                critic_loss = PPO.compute_critic_loss(predicted_state_value[m], target_state_value_seq[sample_seq][m])
+                critic_loss = PPO.compute_critic_loss(state_value_seq[m], target_state_value_seq[sample_seq][m])
                 
                 # train step
                 loss = actor_loss + self.config.value_loss_coef * critic_loss - self.config.entropy_coef * entropy
@@ -203,96 +202,6 @@ class RecurrentPPO(Agent):
                 # log data
                 self.actor_average_loss.update(actor_loss.item())
                 self.critic_average_loss.update(critic_loss.item())
-                
-    def to_batch_sequences(self, 
-                           obs: torch.Tensor, 
-                           action: ActionTensor,
-                           terminated: torch.Tensor,
-                           action_log_prob: torch.Tensor,
-                           advantage: torch.Tensor,
-                           v_target: torch.Tensor,
-                           hidden_state: torch.Tensor,
-                           n_steps: int):
-        # 1. stack sequence_length experiences
-        # 2. when episode is terminated or remained experiences < sequence_length, zero padding
-        # 3. feed forward
-        
-        # batch_size = 128
-        # sequence_length = 8
-        # if not teraminted
-        # stack experiences
-        # if sequence_length is reached or terminated:
-        # stop stacking and go to next sequence
-        mask = torch.ones((self.num_envs, n_steps))
-        
-        b2e = lambda x: drl_util.batch2perenv(x, self.num_envs)
-        obs = b2e(obs)
-        discrete_action = b2e(action.discrete_action) if action.num_discrete_branches > 0 else torch.empty((self.num_envs, n_steps, 0))
-        continuous_action = b2e(action.continuous_action) if action.num_continuous_branches > 0 else torch.empty((self.num_envs, n_steps, 0))
-        terminated = b2e(terminated)
-        action_log_prob = b2e(action_log_prob)
-        advantage = b2e(advantage)
-        v_target = b2e(v_target)
-        # (max_num_layers, batch_size, *out_features) -> (batch_size, max_num_layers, *out_features)
-        hidden_state = hidden_state.swapaxes(0, 1)
-        hidden_state = b2e(hidden_state)
-        
-        sequence_start_hidden_state = []
-        stacked_obs = []
-        stacked_discrete_action = []
-        stacked_continuous_action = []
-        stacked_action_log_prob = []
-        stacked_advantage = []
-        stacked_v_target = []
-        stacked_mask = []
-        
-        seq_len = self.config.sequence_length
-        
-        for env_id in range(self.num_envs):
-            seq_start = 0
-            t = 0
-            terminated_idxes = torch.where(terminated[env_id] > 0.5)[0]
-            
-            while seq_start < n_steps:
-                seq_end = min(seq_start + seq_len, n_steps)
-                
-                # if terminated in the middle of sequence
-                # it will be zero padded
-                if t < len(terminated_idxes) and terminated_idxes[t] < seq_end:
-                    seq_end = terminated_idxes[t].item() + 1
-                    t += 1
-                    
-                sequence_start_hidden_state.append(hidden_state[env_id, seq_start])
-                
-                idx = torch.arange(seq_start, seq_end)
-                stacked_obs.append(obs[env_id, idx])
-                stacked_discrete_action.append(discrete_action[env_id, idx])
-                stacked_continuous_action.append(continuous_action[env_id, idx])
-                stacked_action_log_prob.append(action_log_prob[env_id, idx])
-                stacked_advantage.append(advantage[env_id, idx])
-                stacked_v_target.append(v_target[env_id, idx])
-                stacked_mask.append(mask[env_id, idx])
-                
-                seq_start = seq_end
-
-        # (max_num_layers, *out_features) x num_sequences -> (num_sequences, max_num_layers, *out_features)
-        sequence_start_hidden_state = torch.stack(sequence_start_hidden_state)
-        # (num_sequences, max_num_layers, *out_features) -> (max_num_layers, num_sequences, *out_features)
-        sequence_start_hidden_state.swapaxes_(0, 1)
-        
-        pad = lambda x: pad_sequence(x, batch_first=True, padding_value=self.config.padding_value)
-        
-        stacked_obs = pad(stacked_obs)
-        stacked_discrete_action = pad(stacked_discrete_action)
-        stacked_continuous_action = pad(stacked_continuous_action)
-        stacked_action_log_prob = pad(stacked_action_log_prob)
-        stacked_advantage = pad(stacked_advantage)
-        stacked_v_target = pad(stacked_v_target)
-        stacked_mask = pad(stacked_mask)
-        eps = torch.finfo(torch.float32).eps * 2.0
-        stacked_mask = (stacked_mask < self.config.padding_value - eps) | (stacked_mask > self.config.padding_value + eps)
-        
-        return stacked_obs, stacked_discrete_action, stacked_continuous_action, stacked_action_log_prob, stacked_advantage, stacked_v_target, stacked_mask, sequence_start_hidden_state
 
     def _compute_adavantage_target_state_value(self, exp_batch: RecurrentPPOExperienceBatch) -> Tuple[torch.Tensor, torch.Tensor]:
         """
@@ -308,19 +217,19 @@ class RecurrentPPO(Agent):
         
         # (num_envs, *obs_shape)
         final_next_obs = exp_batch.next_obs[-self.num_envs:]
-        final_hidden_state = self.next_hidden_state
+        final_next_hidden_state = self.next_hidden_state.to(device=self.device)
         
         # feed forward without gradient calculation
         with torch.no_grad():
             # (num_envs, 1, *obs_shape) because sequence length is 1
-            _, final_state_value, _ = self.network.forward(final_next_obs.unsqueeze(1), final_hidden_state.to(device=self.device))
+            _, final_next_state_value_seq, _ = self.network.forward(final_next_obs.unsqueeze(dim=1), final_next_hidden_state)
         
         # (num_envs, 1, 1) -> (num_envs, 1)
-        final_state_value.squeeze_(dim=1)
+        final_next_state_value = final_next_state_value_seq.squeeze_(dim=1)
         # (num_envs x (n_steps + 1), 1)
-        total_state_value = torch.cat([exp_batch.v_pred, final_state_value])
+        total_state_value = torch.cat([exp_batch.v_pred, final_next_state_value])
         
-        # (num_envs * T, 1) -> (num_envs, T, 1) -> (num_envs, T)
+        # (num_envs x T, 1) -> (num_envs, T, 1) -> (num_envs, T)
         total_state_value = drl_util.batch2perenv(total_state_value, self.num_envs).squeeze_(-1) # T = n_steps + 1
         reward = drl_util.batch2perenv(exp_batch.reward, self.num_envs).squeeze_(-1) # T = n_steps
         terminated = drl_util.batch2perenv(exp_batch.terminated, self.num_envs).squeeze_(-1) # T = n_steps
