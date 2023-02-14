5ceaee58f4188691f75fa0e9baf57e1589490cb8
DevSlem <devslem12@gmail.com>
2023-02-15 00:04:14 +0900

    Update util module
    * `IncrementalMeanVarianceFromBatch`

--- aine_drl/util/__init__.py
+++ aine_drl/util/__init__.py
@@ -1,3 +1,4 @@
 from .util_methods import *
 from .logger import *
 from .static_recursive_buffer import StaticRecursiveBuffer
+from .incremental_mean_variance_from_batch import IncrementalMeanVarianceFromBatch
