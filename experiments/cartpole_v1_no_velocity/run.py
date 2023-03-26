468dbd8435ca7627499e486c832add0eaf26c5e2
DevSlem <devslem12@gmail.com>
2023-03-27 00:09:55 +0900

    Update CartPole-v1 No Velocity

--- experiments/cartpole_v1_no_velocity/run.py
+++ experiments/cartpole_v1_no_velocity/run.py
@@ -16,13 +16,16 @@ from gym.core import ObservationWrapper
 import aine_drl
 import aine_drl.agent as agent
 import aine_drl.net as net
-from aine_drl.factory import AgentFactory, AINETrainFactory
-from aine_drl.train import Env, GymEnv
+from aine_drl.factory import (AgentFactory, AINEInferenceFactory,
+                              AINETrainFactory)
+from aine_drl.train import Env, GymEnv, GymRenderableEnv
 
+LEARNING_RATE = 0.001
+GRAD_CLIP_MAX_NORM = 5.0
 
 class CartPoleNoVel(ObservationWrapper):
-    def __init__(self):
-        env = gym.make("CartPole-v1")
+    def __init__(self, render_mode = None):
+        env = gym.make("CartPole-v1", render_mode=render_mode)
         super().__init__(env)
         
         self._obs_mask = np.array([True, False, True, False])
@@ -34,6 +37,7 @@ class CartPoleNoVel(ObservationWrapper):
         
     def observation(self, observation):
         return observation[self._obs_mask]
+    
     
 class CartPoleNoVelRecurrentPPONet(nn.Module, agent.RecurrentPPOSharedNetwork):
     def __init__(self, obs_features, num_actions) -> None:
@@ -106,6 +110,36 @@ class CartPoleNoVelRecurrentPPONet(nn.Module, agent.RecurrentPPOSharedNetwork):
         lstm_hidden_state = lstm_hidden_state.split(lstm_hidden_state.shape[2] // 2, dim=2)  # type: ignore
         return (lstm_hidden_state[0].contiguous(), lstm_hidden_state[1].contiguous())
     
+class CartPoleNoVelNaivePPO(nn.Module, agent.PPOSharedNetwork):
+    def __init__(self, obs_features, num_actions) -> None:
+        super().__init__()
+        
+        self.hidden_features = 64
+        
+        # encoding layer for feature extraction
+        self.encoding_layer = nn.Sequential(
+            nn.Linear(obs_features, 64),
+            nn.ReLU(),
+            nn.Linear(64, 128),
+            nn.ReLU(),
+            nn.Linear(128, self.hidden_features),
+            nn.ReLU()
+        )
+        
+        # actor-critic layer
+        self.actor_layer = aine_drl.CategoricalLayer(self.hidden_features, num_actions)
+        self.crictic_layer = nn.Linear(self.hidden_features, 1)
+        
+    def model(self) -> nn.Module:
+        return self
+    
+    def forward(self, obs: aine_drl.Observation) -> tuple[aine_drl.PolicyDistParam, torch.Tensor]:
+        vector_obs = obs.items[0]
+        encoding = self.encoding_layer(vector_obs)
+        pdparam = self.actor_layer(encoding)
+        state_value = self.crictic_layer(encoding)
+        return pdparam, state_value
+    
 class RecurrentPPOFactory(AgentFactory):
     def make(self, env: Env, config_dict: dict) -> agent.Agent:
         config = agent.RecurrentPPOConfig(**config_dict)
@@ -117,8 +151,8 @@ class RecurrentPPOFactory(AgentFactory):
         
         trainer = aine_drl.Trainer(optim.Adam(
             network.parameters(),
-            lr=3e-4
-        )).enable_grad_clip(network.parameters(), max_norm=5.0)
+            lr=LEARNING_RATE
+        )).enable_grad_clip(network.parameters(), max_norm=GRAD_CLIP_MAX_NORM)
         
         policy = aine_drl.CategoricalPolicy()
         
@@ -129,25 +163,71 @@ class RecurrentPPOFactory(AgentFactory):
             policy,
             env.num_envs
         )
+        
+class NaivePPOFactory(AgentFactory):
+    def make(self, env: Env, config_dict: dict) -> agent.Agent:
+        config = agent.PPOConfig(**config_dict)
+        
+        network = CartPoleNoVelNaivePPO(
+            obs_features=env.obs_shape[0],
+            num_actions=env.action_spec.num_discrete_actions[0]
+        ).to(device=torch.device("cuda" if torch.cuda.is_available() else "cpu"))
+        
+        trainer = aine_drl.Trainer(optim.Adam(
+            network.parameters(),
+            lr=LEARNING_RATE
+        )).enable_grad_clip(network.parameters(), max_norm=GRAD_CLIP_MAX_NORM)
+        
+        policy = aine_drl.CategoricalPolicy()
+        
+        return agent.PPO(
+            config,
+            network,
+            trainer,
+            policy,
+            env.num_envs
+        )
     
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
     parser.add_argument("-i", "--inference", action="store_true", help="inference mode")
-    is_inference = parser.parse_args().inference
+    parser.add_argument("-a", "--agent", type=str, default="recurrent_ppo", help="agent type (recurrent_ppo, naive_ppo)")
+    args = parser.parse_args()
+    is_inference = args.inference
+    agent_type = args.agent
     
-    recurrent_ppo_config_path = "config/samples/cartpole_v1_no_velocity_recurrent_ppo.yaml"
+    recurrent_ppo_config_path = "config/experiments/cartpole_v1_no_velocity_recurrent_ppo.yaml"
+    naive_ppo_config_path = "config/experiments/cartpole_v1_no_velocity_naive_ppo.yaml"
+    
+    match agent_type:
+        case "recurrent_ppo":
+            config_path = recurrent_ppo_config_path
+            agent_factory = RecurrentPPOFactory()
+        case "naive_ppo":
+            config_path = naive_ppo_config_path
+            agent_factory = NaivePPOFactory()
+        case _:
+            raise ValueError("invalid agent type")
     
     if not is_inference:
-        aine_factory =  AINETrainFactory.from_yaml(recurrent_ppo_config_path)
+        aine_factory =  AINETrainFactory.from_yaml(config_path)
         
         env = GymEnv(gym.vector.AsyncVectorEnv([
             lambda: CartPoleNoVel() for _ in range(aine_factory.num_envs)
-        ]))
+        ]), seed=aine_factory.seed)
         
         aine_factory.set_env(env) \
-            .make_agent(RecurrentPPOFactory()) \
+            .make_agent(agent_factory) \
             .ready() \
             .train() \
             .close()
     else:
-        raise NotImplementedError
+        aine_factory = AINEInferenceFactory.from_yaml(config_path)
+        
+        env = GymRenderableEnv(CartPoleNoVel(render_mode="rgb_array"), seed=aine_factory.seed)
+        
+        aine_factory.set_env(env) \
+            .make_agent(agent_factory) \
+            .ready() \
+            .inference() \
+            .close()
